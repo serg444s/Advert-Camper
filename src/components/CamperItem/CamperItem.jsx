503c25a2cf7218b0e60e9d4847689a3603878b73
Serhii Savchenko <sergey.savchenko1303@gmail.com>
2024-08-14 22:06:19 +0300

    css

--- src/components/CamperItem/CamperItem.jsx
+++ src/components/CamperItem/CamperItem.jsx
@@ -4,7 +4,13 @@ import css from "./CamperItem.module.css"
 
 const CamperItem = ({ item }) => {
     
-    
+  const place = "https://www.google.com/maps/place";
+
+  function getLocation(str) {
+    const parts = str.split(',');
+    return parts[1].trim(); 
+  }
+
     return (
       <div className={css.container}>
 
@@ -19,9 +25,19 @@ const CamperItem = ({ item }) => {
             <IconSvg iconName={"hart"} />
         </div>
 
-        <div className={location}>
-        <p>        <IconSvg iconName={"star"} />
+        <div className={css.location}>
+        <p>        <IconSvg width={16} height={16} iconName={"star"} />
         {item.rating} ({item.reviews.length} Reviews)</p>
+
+
+        <a 
+  href={`${place}/${getLocation(item.location)}`} 
+  target="_blank" 
+  rel="noopener noreferrer"
+>
+<IconSvg width={16} height={16} iconName={"location"} />
+{item.location}
+</a>
         </div>
             
             <p className={css.text}>{item.description}</p>
