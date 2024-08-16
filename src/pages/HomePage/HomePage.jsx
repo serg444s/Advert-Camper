e314316f1125b7764bdfb60164e48bdc070b0750
Serhii Savchenko <sergey.savchenko1303@gmail.com>
2024-08-16 23:27:07 +0300

    HomePage

--- src/pages/HomePage/HomePage.jsx
+++ src/pages/HomePage/HomePage.jsx
@@ -1,11 +1,20 @@
-import IconSvg from "../../components/IconSvg/IconSvg";
+import { Link } from "react-router-dom";
+import css from "./HomePage.module.css"
 
 const HomePage = () => { 
-    return <div>
 
-<h2>Home Page</h2>
+    const url = "https://images.prom.ua/3968330539_w640_h640_3968330539.jpg"
 
-<IconSvg iconName={"shower"}/>
+    return <div className={css.container}>
+
+<h1 className={css.title}>Discover the Freedom of the Open Road with Our Camper Rentals</h1>
+<div className={css.wrapper}>
+    <img src={url} alt="rent camper" className={css.img}/>
+</div>
+ <Link to="/catalog" className={css.link}>
+        Select now
+      </Link>
+<p className={css.text}>Welcome to our camper rental service, where your adventure begins! Whether you are planning a weekend getaway or an extended road trip, we offer a wide range of well-maintained campers to suit your needs. Our fleet includes everything from compact vans to spacious motorhomes, all equipped with modern amenities to ensure your comfort. Explore breathtaking landscapes, enjoy the convenience of a home on wheels, and create unforgettable memories. Book your perfect camper today and embark on the journey of a lifetime!</p>
     </div>
 }
 
